0cd2d0cb0c8850d84ddae37597fb46ddbd549d21
Kenny <enriquezkenny1867@gmail.com>
2026-03-16 18:12:06 -0600

    sopa

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ToastProvider } from './components/ui.tsx'
+import { ToastProvider } from './components/ui'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
